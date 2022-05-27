17760189c738da70145dbb1f3c85ec0c1ce8fc2c
NMV <98959079+NicoNMV@users.noreply.github.com>
2022-05-27 11:45:38 -0300

    Add files via upload

---
+++ Pesssoa.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula27_05
+{
+    class Pessoa
+    {
+        private string nome;
+        private int idade;
+        private double altura;
+
+        public Pessoa()
+        {
+            this.nome = null;
+            this.idade = 0;
+            this.altura = 0.0;
+        }
+
+        public Pessoa(string nome, int idade, double altura)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.altura = altura;
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public int Idade
+        {
+            get { return idade; }
+            set { idade = value; }
+        }
+
+        public double Altura
+        {
+            get { return altura; }
+            set { altura = value; }
+        }
+
+        public override string ToString()
+        {
+            return (String.Format("Nome: {0} \n Idade: {1} \n Altura: {2}",
+                                  nome, idade, altura));
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Aula27_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o nome da pessoa: "); string n = Console.ReadLine();
+            Console.Write("Digite a idade de {0}: ", n); int i = int.Parse(Console.ReadLine());
+            Console.Write("Digite a altura de {0}: ", n); double a = double.Parse(Console.ReadLine());
+           
+            Pessoa pessoa = new Pessoa(n, i, a);
+
+            Console.WriteLine("\n\n*** Dados da Pessoa ***\n");
+            Console.WriteLine(pessoa.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
